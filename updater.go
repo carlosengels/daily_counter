82767f0397c72334ab968eace14e0c8cb5f01e40
Carlos Engels <carlose@startmail.com>
2025-01-23 14:32:28 -0700

    add update file functionality

--- updater.go
+++ updater.go
@@ -4,17 +4,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	filePath := "files//counter.txt"
 
 	content, err := os.ReadFile(filePath)
-
 	if err != nil {
 		log.Fatalf("Error reading file : %v", err)
 	}
 
-	fmt.Println("File Content:")
-	fmt.Println(string(content))
+	counterValue, err := strconv.Atoi(string(content))
+	if err != nil {
+		log.Fatalf("Error converting file content to integer: %v", err)
+	}
+
+	counterValue++
+
+	updatedContent := strconv.Itoa(counterValue)
+
+	err = os.WriteFile(filePath, []byte(updatedContent), 0644)
+	if err != nil {
+		log.Fatalf("Error converting file content to integer: %v", err)
+	}
+
+	fmt.Println("Updated File Content:")
+	fmt.Println(string(updatedContent))
 }
